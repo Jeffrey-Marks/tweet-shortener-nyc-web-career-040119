42e53e02b4de597ea6dc0b7a0eee85c7f84c2493
Jeff M <jkdmarks@gmail.com>
2019-03-13 02:55:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,17 +1,23 @@
-Dictionary = {
-  "hello" => "hi",
-  "to" => "2",
-  "two" => "2",
-  "too" => "2",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
-}
-
-
 def word_substitutor(tweet)
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+
   tweet = tweet.split(" ")
-  
-  
+  i = 0
+
+  while i < tweet.length
+    if dictionary.keys.include?(tweet[i])
+      tweet[i] = dictionary[tweet[i]] #tweet[i] = dictionary(tweet[i])
+    end
+    i += 1
+  end
+  tweet.join(" ")
 end
